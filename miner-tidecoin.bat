fc30ce0363a6cd3ae09b1418b243d23ea37bcacf
gretisane <92603514+gretisane@users.noreply.github.com>
2021-11-17 11:21:27 +0700

    Update miner-tidecoin.bat

--- miner-tidecoin.bat
+++ miner-tidecoin.bat
@@ -6,5 +6,5 @@ rem       stratum-asia.rplant.xyz /Singapore/
 rem       stratum-na.rplant.xyz   /Toronto/
 rem
 :start
-"%~dp0"cpuminer-sse2.exe -a yespowerTIDE -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u WALLET_ADDRESS.WORKER_NAME
+"%~dp0"cpuminer-sse2.exe -a yespowerTIDE -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.Windows
 goto start
